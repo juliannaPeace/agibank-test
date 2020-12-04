7ecf8d2a6272535f4608f63effb342ab127d4989
Julianna Paz <julianna.plo@grupocev.com>
2020-12-04 16:32:23 -0300

    Criação do Projeto

---
+++ src/main/java/com/julianna/agibanktest/AgibankTestApplication.java
@@ -0,0 +1,13 @@
+package com.julianna.agibanktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgibankTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgibankTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julianna/agibanktest/AgibankTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianna.agibanktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgibankTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
