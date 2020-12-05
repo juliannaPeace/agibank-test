1c67520920653e1334262d8d55eed54abbb5d652
Julianna Paz <julianna.plo@grupocev.com>
2020-12-05 12:06:30 -0300

    exclusão de MapEntity e criação da classe Util

--- src/main/java/com/julianna/agibanktest/utils/Util.java
+++ src/main/java/com/julianna/agibanktest/utils/Util.java
@@ -1,5 +1,7 @@
 package com.julianna.agibanktest.utils;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,8 +10,9 @@ import com.julianna.agibanktest.entities.Item;
 import com.julianna.agibanktest.entities.Vendedor;
 import com.julianna.agibanktest.interfaces.TipoDado;
 
-public abstract class MapEntity {
-
+public class Util {
+	
+	
 	public static Map<String, TipoDado> getMapEntities() {
 
 		Map<String, TipoDado> mapEntities = new HashMap<>();
@@ -23,5 +26,18 @@ public abstract class MapEntity {
 
 	public static Object gerarEntidades(TipoDado tipoDado, String value1, String value2, String value3) {
 		return tipoDado.gerarEntidade(value1, value2, value3);
+	}
+
+	public static void createFile(StringBuilder stringBuilder, String path, String fileName) {
+
+		FileWriter file = null;
+		try {
+			file = new FileWriter(path + fileName);
+			file.write(stringBuilder.toString());
+			file.flush();
+			file.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 }
