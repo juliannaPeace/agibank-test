2dd7c0bbc81dcf4873cbb8e0b53253b86617012c
Julianna Paz <julianna.plo@grupocev.com>
2020-12-05 10:59:43 -0300

    Criando os diretórios de entrada e saída de dados.

--- src/main/java/com/julianna/agibanktest/AgibankTestApplication.java
+++ src/main/java/com/julianna/agibanktest/AgibankTestApplication.java
@@ -1,13 +1,34 @@
 package com.julianna.agibanktest;
 
+import java.io.File;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.julianna.agibanktest.utils.Constantes;
+
 @SpringBootApplication
-public class AgibankTestApplication {
+public class AgibankTestApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgibankTestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		createWithNotExistHomePathIn();
+		createWithNotExistHomePathOut();
+	}
+	
+	private void createWithNotExistHomePathIn() {
+		File file = new File(Constantes.HOMEPATH_IN);
+		file.mkdirs();
+	}
+	
+	private void createWithNotExistHomePathOut() {
+		File file = new File(Constantes.HOMEPATH_OUT);
+		file.mkdirs();
 	}
 
 }
--- src/main/java/com/julianna/agibanktest/utils/Constantes.java
+++ src/main/java/com/julianna/agibanktest/utils/Constantes.java
@@ -4,4 +4,5 @@ public class Constantes {
 
 	public static String HOMEPATH = System.getProperty("user.home");
 	public static String HOMEPATH_OUT = HOMEPATH + "/data/out/";
+	public static String HOMEPATH_IN = HOMEPATH + "/data/in/";
 }
