b202c4677a2623e7be35bae6c2b26b22248ef1fa
Julianna Paz <julianna.plo@grupocev.com>
2020-12-05 16:30:57 -0300

    Upload de múltiplos arquivos.

--- src/main/java/com/julianna/agibanktest/controllers/UploadFileController.java
+++ src/main/java/com/julianna/agibanktest/controllers/UploadFileController.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,27 +26,35 @@ public class UploadFileController {
 	}
 
 	@PostMapping("/upload")
-	public String singleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+	public String multipleFileUpload(@RequestParam("files") MultipartFile[] files,
+			RedirectAttributes redirectAttributes) {
 
-		if (file.isEmpty()) {
+		StringBuilder filesUpload = new StringBuilder();
+		
+		if (Arrays.asList(files).isEmpty()) {
 			redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
 			return "redirect:uploadStatus";
 		}
 
-		try {
+		Arrays.asList(files).stream().forEach(file -> {
+			try {
 
-			byte[] bytes = file.getBytes();
+				byte[] bytes = file.getBytes();
 
-			Path path = Paths.get(Constantes.HOMEPATH_IN + file.getOriginalFilename());
+				Path path = Paths.get(Constantes.HOMEPATH_IN + file.getOriginalFilename());
 
-			Files.write(path, bytes);
+				Files.write(path, bytes);
+				
+				filesUpload.append(file.getOriginalFilename());
+				filesUpload.append(System.getProperty("line.separator"));
 
-			redirectAttributes.addFlashAttribute("message",
-					"You successfully uploaded '" + file.getOriginalFilename() + "'");
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+		
+		redirectAttributes.addFlashAttribute("message",
+				"You successfully uploaded '" + filesUpload.toString() + "'");
 
 		return "redirect:/uploadStatus";
 	}
