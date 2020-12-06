fe678192f77a46f5356d67e8cf35ddfcfa54f1ff
Julianna Paz <julianna.plo@grupocev.com>
2020-12-06 17:49:07 -0300

    Listagem de arquivos processados.

--- src/main/java/com/julianna/agibanktest/controllers/UploadFileController.java
+++ src/main/java/com/julianna/agibanktest/controllers/UploadFileController.java
@@ -1,27 +1,38 @@
 package com.julianna.agibanktest.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.julianna.agibanktest.entities.FileOut;
+import com.julianna.agibanktest.services.ProcessFileService;
 import com.julianna.agibanktest.utils.Constantes;
 
 @Controller
 public class UploadFileController {
 
+	@Autowired
+	private ProcessFileService processFileService;
+
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+
+		listAllFilesPathOut(model);
+
 		return "upload";
 	}
 
@@ -30,9 +41,9 @@ public class UploadFileController {
 			RedirectAttributes redirectAttributes) {
 
 		StringBuilder filesUpload = new StringBuilder();
-		
+
 		if (Arrays.asList(files).isEmpty()) {
-			redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+			redirectAttributes.addFlashAttribute("message", "Por favor selecione arquivo para upload");
 			return "redirect:uploadStatus";
 		}
 
@@ -44,7 +55,7 @@ public class UploadFileController {
 				Path path = Paths.get(Constantes.HOMEPATH_IN + file.getOriginalFilename());
 
 				Files.write(path, bytes);
-				
+
 				filesUpload.append(file.getOriginalFilename());
 				filesUpload.append(System.getProperty("line.separator"));
 
@@ -52,11 +63,19 @@ public class UploadFileController {
 				e.printStackTrace();
 			}
 		});
-		
+
 		redirectAttributes.addFlashAttribute("message",
-				"You successfully uploaded '" + filesUpload.toString() + "'");
+				"Upload de arquivo feito com sucesso '" + filesUpload.toString() + "'");
 
 		return "redirect:/uploadStatus";
+	}
+
+	private void listAllFilesPathOut(Model model) {
+		Optional<File> file = Optional.ofNullable(new File(Constantes.HOMEPATH_OUT));
+
+		List<FileOut> listFileOut = processFileService.listAllFilesPathOut(file);
+
+		model.addAttribute("files", listFileOut);
 	}
 
 	@GetMapping("/uploadStatus")
